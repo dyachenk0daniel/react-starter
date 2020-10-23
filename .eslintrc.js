725055a564d644f99df8b5f0dc6d3e964e26d8b5
danieldyachenko <daniildyachenko95@gmail.com>
2020-10-23 16:07:45 +0300

    ESLint 1.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier",
     "prettier/@typescript-eslint",
