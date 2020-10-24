519d7229724e4e76c5e5560c1ee0cbac118b2da7
danieldyachenko <daniildyachenko95@gmail.com>
2020-10-24 11:14:53 +0300

    add prettier ignor

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,48 +4,51 @@ module.exports = {
     browser: true,
     node: true,
   },
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  plugins: ["@typescript-eslint", "react-hooks"],
+  ignorePatterns: ["/node_modules/*", "/build/*"],
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "prettier/babel",
-    "prettier/react",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
   ],
   rules: {
-    "react-hooks/rules-of-hooks": 2,
-    "react-hooks/exhaustive-deps": 1,
-    "react/prop-types": 0,
-    "@typescript-eslint/no-var-requires": 0, //использование операторов require
-    "no-console": 1, //запретить использование `console`
-    "no-extra-parens": 1, //запретить ненужные скобки
-    "array-callback-return": 1, //применять операторы return в обратных вызовах методов массива
-    "default-case": 1, //требовать регистры default в операторах switch
-    eqeqeq: 1, // требуют использования `===` и `! ==`
-    "no-alert": 1, // запретить использование ʻalert`, `confirm` и` prompt`
-    "no-constructor-return": 1, // запретить возвращение значения из конструктора
-    "no-else-return": 1, // запретить блоки ʻelse` после операторов return в операторах ʻif`
-    "no-empty-function": 1, // запретить пустые функции
-    "no-eq-null": 1, // запретить сравнения null без операторов проверки типов
-    "no-extra-bind": 1, // запретить ненужные вызовы `.bind ()`
-    "no-lone-blocks": 1, //запретить ненужные вложенные блоки
-    "no-multi-spaces": 1, //запретить использование нескольких пробелов
-    "no-unused-expressions": 1, //запретить неиспользуемые выражения
-    "require-await": 2, //запретить async функции, у которых нет выражения await
+    'react-hooks/rules-of-hooks': 2, //Проверяет правила хуков
+    'react-hooks/exhaustive-deps': 1, //Проверяет зависимости эффектов
+    'react/prop-types': 0,
+    '@typescript-eslint/no-var-requires': 0, //использование операторов require
+    'no-console': 1, //запретить использование `console`
+    'no-extra-parens': 1, //запретить ненужные скобки
+    'array-callback-return': 1, //применять операторы return в обратных вызовах методов массива
+    'default-case': 1, //требовать регистры default в операторах switch
+    'eqeqeq': 1, // требуют использования `===` и `! ==`
+    'no-alert': 1, // запретить использование ʻalert`, `confirm` и` prompt`
+    'no-constructor-return': 1, // запретить возвращение значения из конструктора
+    'no-else-return': 1, // запретить блоки ʻelse` после операторов return в операторах ʻif`
+    'no-empty-function': 1, // запретить пустые функции
+    'no-eq-null': 1, // запретить сравнения null без операторов проверки типов
+    'no-extra-bind': 1, // запретить ненужные вызовы `.bind ()`
+    'no-lone-blocks': 1, //запретить ненужные вложенные блоки
+    'no-multi-spaces': 1, //запретить использование нескольких пробелов
+    'no-unused-expressions': 1, //запретить неиспользуемые выражения
+    'require-await': 2, //запретить async функции, у которых нет выражения await
   },
   settings: {
     // общие настройки
     react: {
-      pragma: "React",
-      version: "detect",
+      pragma: 'React',
+      version: 'detect',
     },
   },
   reportUnusedDisableDirectives: true, //сообщить о неиспользованных eslint-disable комментариях
--- webpack.config.js
+++ webpack.config.js
@@ -1,118 +1,120 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-  mode: "development",
-  //mode: "production",
-  target: "web",
-  entry: {
-    index: "./src/index.tsx",
-  },
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].bundle.js",
-    // publicPath: '/',
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"], // Расширения, которые используются
-  },
-  devServer: {
-    index: "index.html",
-    contentBase: path.join(__dirname, "public"), // Расположение статических файлов
-    watchContentBase: true, // Изменение файлов вызывает полную перезагрузку страницы.
-    compress: true, // Включить сжатие gzip
-    hot: true, // Горячая замена модуля
-    open: true,
-    port: 4200,
-    // noInfo: true, // Только ошибки и предупреждения о горячей перезагрузке
-  },
-  devtool: "inline-source-map",
-  //devtool: false,
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|tsx|ts)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+module.exports = (env, argv) => {
+  return {
+    target: 'web',
+    entry: {
+      index: './src/index.tsx',
+    },
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].bundle.js',
+      // publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'], // Расширения, которые используются
+    },
+    devServer: {
+      index: 'index.html',
+      contentBase: path.join(__dirname, 'public'), // Расположение статических файлов
+      watchContentBase: true, // Изменение файлов вызывает полную перезагрузку страницы.
+      //compress: true, // Включить сжатие gzip
+      hot: true, // Горячая замена модуля
+      open: true,
+      port: 4200,
+      // noInfo: true, // Только ошибки и предупреждения о горячей перезагрузке
+    },
+    devtool:
+      argv.mode === 'development'
+        ? 'inline-source-map'
+        : argv.mode === 'production' && false,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|tsx|ts)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
         },
-      },
-      {
-        test: /\.((c|sa|sc)ss)$/i, //For Less - /\.((c|le)ss)$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 2,
-              modules: {
-                auto: true, ///\.module\.\w+$/i.test(filename)
-                exportGlobals: true,
-                localIdentName: "[path][name]__[local]--[hash:base64:5]",
+        {
+          test: /\.((c|sa|sc)ss)$/i, //For Less - /\.((c|le)ss)$/i,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 2,
+                modules: {
+                  auto: true, ///\.module\.\w+$/i.test(filename)
+                  exportGlobals: true,
+                  localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                },
               },
             },
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: ["postcss-preset-env"],
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: ['postcss-preset-env'],
+                },
               },
             },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "images",
+            {
+              loader: 'sass-loader',
             },
-          },
-        ],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "fonts",
+          ],
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'images',
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      },
-    ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: "public/index.html",
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: "public/robots.txt" },
-        { from: "public/favicon.ico" },
-        { from: "public/manifest.json" },
-        { from: "public/config.js" },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[contenthash].[ext]',
+                outputPath: 'fonts',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: ['@svgr/webpack'],
+        },
       ],
-    }),
-    new ESLintPlugin({
-      extensions: ["ts", "tsx", "js", "jsx"],
-    }),
-  ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html',
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: 'public/robots.txt' },
+          { from: 'public/favicon.ico' },
+          { from: 'public/manifest.json' },
+          { from: 'public/config.js' },
+        ],
+      }),
+      new ESLintPlugin({
+        extensions: ['ts', 'tsx', 'js', 'jsx'],
+      }),
+    ],
+  };
 };
