d6b24bad5761bdf8333130f3ef4c6ff0f8a3d9ad
danieldyachenko <daniildyachenko95@gmail.com>
2020-11-15 21:45:48 +0300

    add scss functions.scss, new font and heartbeat animation

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import ErrorHandling from './components/ErrorHandling/ErrorHandling';
+
+import './styles/globals.scss'
 
 const rootContainer = document.querySelector('body > #root');
 
